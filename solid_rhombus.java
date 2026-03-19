5322b7db7a17c9e28c94586969957cccfd6e6fa5
Ritik Roushan <ritikroushan777@gmail.com>
2026-03-19 16:46:52 +0530

    solid_rhombus

--- solid_rhombus.java
+++ solid_rhombus.java
@@ -5,11 +5,7 @@ public class solid_rhombus {
             System.out.print(" ");
           }
           for(int j=1;j<=n;j++){
-            if(i==1 || i==n ||j==1 || j==n){
-                System.out.print("*");
-            }else{
-                System.out.print(" ");
-            }
+           System.out.print("*");
           }
           System.out.println();
         }

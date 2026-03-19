c78652f6eac24b7fc31955ec73e0fa8ad2a17ea6
Ritik Roushan <ritikroushan777@gmail.com>
2026-03-19 16:33:54 +0530

    zero one triangle

--- zero_1_triangle.java
+++ zero_1_triangle.java
@@ -1,6 +1,5 @@
 public class zero_1_triangle {
     public static void pattern(int n){
-        int num=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                if((i+j)%2==0){
